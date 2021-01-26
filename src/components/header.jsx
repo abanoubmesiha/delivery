5be0761913e54aa794fe01e54dc50ca56ac30e5c
Abanoub Ayoub <abanoub@terkwaz.com>
2021-01-26 22:39:02 +0200

    Add header component
    * Add `meta-normal` font.

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="logo"/>
+            <label>Home</label>
+            <label>Services</label>
+            <label>About</label>
+            <label>How It Works</label>
+            <label>Testimonials</label>
+        </div>
+    )
+}
