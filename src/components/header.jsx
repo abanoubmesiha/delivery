1aa3c58a6e2eb2254f6620a2f0700a68dd0c92af
Abanoub Ayoub <abanoub@terkwaz.com>
2021-01-27 20:48:02 +0200

    Improve UI of `header` and `home`

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,14 +5,16 @@ export default function Header() {
     return (
         <header>
             <div className="row d-flex align-items-center justify-content-center m-0">
-                <div className="col-5 text-center">
+                <div className="col-6 text-center">
                     <img src={logo} alt="logo" />
                 </div>
-                <div className="col-1">Home</div>
-                <div className="col-1">Services</div>
-                <div className="col-1">About</div>
-                <div className="col-1">How It Works</div>
-                <div className="col-1">Testimonials</div>
+                <div className="offset-1 col">
+                    <label>Home</label>
+                    <label>Services</label>
+                    <label>About</label>
+                    <label>How It Works</label>
+                    <label>Testimonials</label>
+                </div>
             </div>
         </header>
     )
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 export default function Home() {
     return (
         <main>
-            
+            <article>
+                <div className="d-flex">
+                    <h1 className="strong">Home Delivery</h1>
+                    <h1>&nbsp;</h1><h1>From</h1>
+                </div>
+                <h1>Stores Near You</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend eros ut lacus rhoncus vestibulum.
+                </p>
+            </article>
         </main>
     )
 }
