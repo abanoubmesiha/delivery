6f208079fbad008d088ec065e97271abadfdc991
Abanoub Ayoub <abanoub@terkwaz.com>
2021-02-01 23:15:24 +0200

    Make header responsive to mobile

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,16 +5,26 @@ export default function Header() {
     return (
         <header>
             <div className="row d-flex align-items-center justify-content-center m-0">
-                <div className="col-6 text-center">
+                <div className="col-sm-12 col-lg-5 text-center">
                     <img src={logo} alt="logo" />
                 </div>
-                <div className="offset-1 col">
+                <div className="col-sm-0 col-lg-1" />
+                <div className="col-sm-12 col-lg-auto text-center">
                     <a href="#Home">Home</a>
+                </div>
+                <div className="col-sm-12 col-lg-auto text-center">
                     <a href="#Services">Services</a>
+                </div>
+                <div className="col-sm-12 col-lg-auto text-center">
                     <a href="#About">About</a>
+                </div>
+                <div className="col-sm-12 col-lg-auto text-center">
                     <a href="#How">How It Works</a>
+                </div>
+                <div className="col-sm-12 col-lg-auto text-center">
                     <a href="#Testimonials">Testimonials</a>
                 </div>
+                <div className="col-sm-12 col-lg-1"></div>
             </div>
         </header>
     )
