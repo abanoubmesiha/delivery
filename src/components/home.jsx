76e576eb261d0762c8a4ab4e8821900adad374d5
Abanoub Ayoub <abanoub@terkwaz.com>
2021-02-02 00:23:31 +0200

    Make Home section responsive.

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,14 +4,22 @@ export default function Home() {
     return (
         <main>
             <article>
-                <div className="d-flex">
-                    <h1 className="strong">Home Delivery</h1>
-                    <h1>&nbsp;</h1><h1>From</h1>
+                <div className="row">
+                    <div className="col d-flex">
+                        <h1 className="strong">Home Delivery</h1>
+                        <h1>&nbsp;</h1><h1>From</h1>
+                    </div>
                 </div>
-                <h1>Stores Near You</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend eros ut lacus rhoncus vestibulum. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet.
-                </p>
+                <div className="row">
+                    <div className="col">
+                        <h1>Stores Near You</h1>
+                    </div>
+                </div>
+                <div className="row mb-5">
+                    <div className="col-sm-12 col-md-6">
+                        Lorem idivsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend eros ut lacus rhoncus vestibulum. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet.
+                    </div>
+                </div>
                 <button className="red-btn">Download App</button>
             </article>
         </main>
