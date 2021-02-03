a9c8142d5c955ff60e2723c294fe6e8296e33572
Abanoub Ayoub <abanoub@terkwaz.com>
2021-02-04 00:13:54 +0200

    Update `home` to grid sys.

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -5,25 +5,34 @@ export default function Home(props) {
     const { home } = props.data;
     return (
         <div id="Home">
-            <section>
-                <div className="row">
-                    <div className="col d-flex">
-                        <h1 className="strong">{home.titleBold}</h1>
-                        <h1>&nbsp;</h1><h1>{home.afterTitle}</h1>
+            <div className="row d-flex align-items-center with-leaf">
+                <div className="col-sm-12 col-lg-6">
+                    <div className="row">
+                        <div className="offset-sm-1 offset-lg-3 col d-flex">
+                            <h1 className="strong">{home.titleBold}</h1>
+                            <h1>&nbsp;</h1><h1>{home.afterTitle}</h1>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="offset-sm-1 offset-lg-3 col">
+                            <h1>{home.underTitle}</h1>
+                        </div>
+                    </div>
+                    <div className="row mb-5">
+                        <div className="offset-sm-1 offset-lg-3 col">
+                            {home.description}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="offset-sm-1 offset-lg-3 col">
+                            <button className="red-btn">{home.buttonLabel}</button>
+                        </div>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col">
-                        <h1>{home.underTitle}</h1>
-                    </div>
+                <div className="col-sm-12 col-lg-6 py-5">
+                    <img src={require(`../../assets/images/hero/${home.img}`).default} alt="home" />
                 </div>
-                <div className="row mb-5">
-                    <div className="col-sm-12 col-md-6">
-                        {home.description}
-                    </div>
-                </div>
-                <button className="red-btn">{home.buttonLabel}</button>
-            </section>
+            </div>
         </div>
     )
 }
