9fbde15451f90e3957ed2bca2b10e851c4604a37
Abanoub Ayoub <abanoub@terkwaz.com>
2021-02-08 00:16:46 +0200

    Change padding from `margin` CSS property to bootstrap grid.
    * Remove `.container-fluid` override style.

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -8,43 +8,47 @@ export default function About(props) {
     return (
         <div id="About">
             <section>
-                <div className="row mx-5">
-                    <div className="col-sm-12 col-lg-6">
-                        <img src={require(`../../assets/images/about/${about.banner}`).default} alt="about-banner" />
-                    </div>
-                    <div className="col-sm-12 col-lg-6">
-                        <h5 className="strong mb-4">{about.title}</h5>
-                        <h2 className="strong mb-4">{about.secondTitle}</h2>
-                        <p>{about.description}</p>
-                        <h6 className="strong d-inline">Rating {about.rating}</h6>
-                        {Array(Math.round(about.rating)).fill(<FontAwesomeIcon icon={faStar} />)}
-                        <div className="row mt-4 mx-0">
-                            <div className="col red-square">
-                                <div className="row">
-                                    {about.statistics.map((data, i)=>{
-                                        const { length } = about.statistics;
-                                        let { value } = data;
-                                        if (value >= 1000000){
-                                            value = value / 1000000
-                                            value = `${Math.round(value)}M+` 
-                                        } else if (value > 1000){
-                                            value = value / 1000
-                                            value = `${Math.round(value)}K+` 
-                                        } 
-                                        return (
-                                        <div className={`col-sm-12 col-lg-${Math.round(12/length)} ${length-1 === i?'':'border-after'}
-                                         d-flex justify-content-center`}>
-                                            <div>
-                                                <h3 className="text-white strong">{value}</h3>
-                                                <h6 className="text-white strong">{data.label}</h6>
+            <div className="row d-flex justify-content-center">
+                <div className="col-sm-10 col-lg-9">
+                    <div className="row">
+                        <div className="col-sm-12 col-lg-6">
+                            <img src={require(`../../assets/images/about/${about.banner}`).default} alt="about-banner" />
+                        </div>
+                        <div className="col-sm-12 col-lg-6">
+                            <h5 className="strong mb-4">{about.title}</h5>
+                            <h2 className="strong mb-4">{about.secondTitle}</h2>
+                            <p>{about.description}</p>
+                            <h6 className="strong d-inline">Rating {about.rating}</h6>
+                            {Array(Math.round(about.rating)).fill(<FontAwesomeIcon icon={faStar} />)}
+                            <div className="row mt-4 mx-0">
+                                <div className="col red-square">
+                                    <div className="row">
+                                        {about.statistics.map((data, i)=>{
+                                            const { length } = about.statistics;
+                                            let { value } = data;
+                                            if (value >= 1000000){
+                                                value = value / 1000000
+                                                value = `${Math.round(value)}M+` 
+                                            } else if (value > 1000){
+                                                value = value / 1000
+                                                value = `${Math.round(value)}K+` 
+                                            } 
+                                            return (
+                                            <div className={`col-sm-12 col-lg-${Math.round(12/length)} ${length-1 === i?'':'border-after'}
+                                            d-flex justify-content-center`}>
+                                                <div>
+                                                    <h3 className="text-white strong">{value}</h3>
+                                                    <h6 className="text-white strong">{data.label}</h6>
+                                                </div>
                                             </div>
-                                        </div>
-                                    )})}
+                                        )})}
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
             </section>
         </div>
     )
--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -8,18 +8,22 @@ export default function Services(props) {
             <section>
                 <h5 className="strong text-center">Delivery Services</h5>
                 <h2 className="strong text-center mb-5">All Essentials You Need</h2>
-                <div className="row mx-5">
-                    {services.map((service, i) => (
-                        <div key={i} className="col-sm-12 col-lg-4 mb-5 text-left">
-                            <div className="card">
-                                <img src={require(`../../assets/images/services/${service.img}`).default} className="card-img-top" alt={service.title} />
-                                <div className="card-body">
-                                    <h5 className="card-title strong">{service.title}</h5>
-                                    <p className="card-text">{service.description}</p>
+                <div className="row d-flex justify-content-center">
+                    <div className="col-sm-11 col-lg-9">
+                        <div className="row">
+                            {services.map((service, i) => (
+                                <div key={i} className="col-sm-12 col-lg-4 mb-5 text-left">
+                                    <div className="card">
+                                        <img src={require(`../../assets/images/services/${service.img}`).default} className="card-img-top" alt={service.title} />
+                                        <div className="card-body">
+                                            <h5 className="card-title strong">{service.title}</h5>
+                                            <p className="card-text">{service.description}</p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
+                            ))}
                         </div>
-                    ))}
+                    </div>
                 </div>
             </section>
         </div>
