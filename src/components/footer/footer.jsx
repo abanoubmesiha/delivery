e5b81aa2b46fa75da0d19f906b89c9b304632287
Abanoub Ayoub <abanoub@terkwaz.com>
2021-02-11 02:01:01 +0200

    * Add Links.
    * Add flower image.
    
    * Add arrow up icon.

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
-import { RiGooglePlayLine, AiOutlineApple } from 'react-icons/all'
+import { RiGooglePlayLine, AiOutlineApple, MdKeyboardArrowUp } from 'react-icons/all'
 import '../../assets/styles/components/footer.sass'
+
+const Section = (header, data, className) => (
+    <div className={"col-md-12 col-lg-2 mb-3 " + className}>
+        <div className="row">
+            <div className="col">
+                <h4>{header}</h4>
+            </div>
+        </div>
+        {data.map((link, i)=>(
+            <div key={i} className="row">
+                <div className="col">
+                    <a className="weak" style={{fontSize: "0.8rem"}} href={link.href}>{link.label}</a>
+                </div>
+            </div>
+        ))}
+    </div>
+)
 
 export default function Footer(props) {
     const { footer } = props;
     return (
-        <footer id="Footer" className="row bg-dark delivery-man">
-            <div className="col-sm-12 col-lg-auto logo-section">
+        <footer id="Footer" className="row bg-dark images d-flex align-items-center">
+            <div className="col-sm-12 col-lg-auto logo-section mb-3">
                 <div className="row mb-2">
                     <div className="col">
                         <img
@@ -47,6 +64,12 @@ export default function Footer(props) {
                     </div>
                 </div>
             </div> 
+            {Section("About Us", footer.aboutUsLinks, "offset-lg-1")}
+            {Section("About", footer.aboutLinks)}
+            {Section("Support", footer.supportLinks)}
+            <a href="/#" className="red-square">
+                <MdKeyboardArrowUp color="white" size={30} />
+            </a>
         </footer>
     )
 }
